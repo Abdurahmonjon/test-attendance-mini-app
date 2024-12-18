96e8b3c228c4df694eab4c428c55d272ff77f4f7
Abdurahmonjon <abdurahmonjonobiddinov@gmail.com>
2024-12-19 00:11:47 +0500

    added requirements

--- attendanceV4.1.py
+++ attendanceV4.1.py
@@ -5,7 +5,7 @@ TOKEN = '7309821581:AAHodQcZRGFlAYbPPbkd6_NBgEzqZr--Yi4'
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [KeyboardButton("Dasturchaga kirish", web_app={"url": "https://abdurahmonjon.github.io/test-attendance-mini-app/frontend/mainpage.html"})]
+        [KeyboardButton("Dasturchaga kirish", web_app={"url": "https://abdurahmonjon.github.io/file-receiver-bot-v2/front.html"})]
     ]
     await update.message.reply_text(
         "Mini App`ni ochish uchun pastdagi tugmachani bosing",
