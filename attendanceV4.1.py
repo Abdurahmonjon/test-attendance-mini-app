99bf15400f2cbf0df68c411efb8f0d4bb248ef76
Abdurahmonjon <abdurahmonjonobiddinov@gmail.com>
2024-12-28 10:30:26 +0500

    removed snow affect

--- attendanceV4.1.py
+++ attendanceV4.1.py
@@ -1,15 +1,16 @@
-from telegram import Update, BotCommand, ReplyKeyboardMarkup, KeyboardButton
+from telegram import Update,InlineKeyboardButton,InlineKeyboardMarkup, BotCommand, ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 TOKEN = '7309821581:AAHodQcZRGFlAYbPPbkd6_NBgEzqZr--Yi4'
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [KeyboardButton("Dasturchaga kirish", web_app={"url": "https://abdurahmonjon.github.io/file-receiver-bot-v2/front.html"})]
+        [InlineKeyboardButton("Dasturchaga kirish", web_app={"url": "https://abdurahmonjon.github.io/test-attendance-mini-app/frontend/mainpage.html"})]
     ]
     await update.message.reply_text(
         "Mini App`ni ochish uchun pastdagi tugmachani bosing",
-        reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+        reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
 def main():
